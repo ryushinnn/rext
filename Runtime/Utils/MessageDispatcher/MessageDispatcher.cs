48ea979930642e7b7b85a0f748502246f8c7a1fc
tienassassin <>
2024-07-17 12:27:07 +0700

    change messagedispatcher method name

--- Runtime/Utils/MessageDispatcher/MessageDispatcher.cs
+++ Runtime/Utils/MessageDispatcher/MessageDispatcher.cs
@@ -12,7 +12,7 @@ namespace Assassin.Utils {
         /// Register callback to a message
         /// </summary>
         /// <param name="callback">function that will be executed when the message is triggered</param>
-        public static void Register(T callback) {
+        public static void AddListener(T callback) {
             _handle = (T)Delegate.Combine(_handle, callback);
         }
 
@@ -20,17 +20,8 @@ namespace Assassin.Utils {
         /// Unregister callback from a message
         /// </summary>
         /// <param name="callback">function that is no longer executed when the message is triggered</param>
-        public static void Unregister(T callback) {
+        public static void RemoveListener(T callback) {
             _handle = (T)Delegate.Remove(_handle, callback);
         }
-    }
-
-    /// <summary>
-    /// Create a new class with format (and name too, recommended) similar to below
-    /// </summary>
-    public static class MessageID {
-        public delegate void Example1();
-        public delegate void Example2(int number);
-        public delegate void Example3(params float[] floats);
     }
 }
